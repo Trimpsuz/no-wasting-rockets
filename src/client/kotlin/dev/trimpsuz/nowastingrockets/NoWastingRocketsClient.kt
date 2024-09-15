bc6eb6c6ec3c06d737f767588efb02406f69632e
Trimpsuz <me@trimpsuz.dev>
2024-09-15 03:03:41 +0300

    Initial commit

---
+++ src/client/kotlin/dev/trimpsuz/nowastingrockets/NoWastingRocketsClient.kt
@@ -0,0 +1,9 @@
+package dev.trimpsuz.nowastingrockets
+
+import net.fabricmc.api.ClientModInitializer
+
+object NoWastingRocketsClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
